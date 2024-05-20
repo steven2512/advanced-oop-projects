709cf94a3235b8577eb04b997ffb2d5944853b3f
Nguyen <nguyenduong251202@gmail.com>
2024-05-20 16:29:38 +1000

    Added Rattata, Spearow, Ekans, Pikachu, Sandshrew, and Nidoran(M) classes with base stats and evolutions

--- pokemon.py
+++ pokemon.py
@@ -79,3 +79,81 @@ class Pidgey(Pokemon):
         self.defence = 8
         self.speed = 56
 
+class Rattata(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.health = 30
+        self.level = 1
+        self.poketype = PokeType.NORMAL
+        self.battle_power = 15
+        self.evolution_line = ["Rattata", "Raticate"]
+        self.name = "Rattata"
+        self.experience = 0
+        self.defence = 5
+        self.speed = 72
+
+class Spearow(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.health = 40
+        self.level = 1
+        self.poketype = PokeType.FLYING
+        self.battle_power = 19
+        self.evolution_line = ["Spearow", "Fearow"]
+        self.name = "Spearow"
+        self.experience = 0
+        self.defence = 9
+        self.speed = 70
+
+class Ekans(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.health = 35
+        self.level = 1
+        self.poketype = PokeType.POISON
+        self.battle_power = 15
+        self.evolution_line = ["Ekans", "Arbok"]
+        self.name = "Ekans"
+        self.experience = 0
+        self.defence = 8
+        self.speed = 55
+
+class Pikachu(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.health = 35
+        self.level = 1
+        self.poketype = PokeType.ELECTRIC
+        self.battle_power = 30
+        self.evolution_line = ["Pikachu", "Raichu"]
+        self.name = "Pikachu"
+        self.experience = 0
+        self.defence = 15
+        self.speed = 90
+
+class Sandshrew(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.health = 50
+        self.level = 1
+        self.poketype = PokeType.GROUND
+        self.battle_power = 30
+        self.evolution_line = ["Sandshrew", "Sandslash"]
+        self.name = "Sandshrew"
+        self.experience = 0
+        self.defence = 20
+        self.speed = 40
+
+class NidoranM(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.health = 46
+        self.level = 1
+        self.poketype = PokeType.POISON
+        self.battle_power = 23
+        self.evolution_line = ["Nidoran(M)", "Nidorino", "Nidoking"]
+        self.name = "Nidoran(M)"
+        self.experience = 0
+        self.defence = 7
+        self.speed = 41
+
